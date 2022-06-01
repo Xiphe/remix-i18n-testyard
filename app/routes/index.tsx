bc51c2e1259627f617c557893d751e32267412bb
Xiphe <github@xiphe.net>
2022-06-01 14:03:26 +0200

    feat: install i18n

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,12 @@
-export default function Index() {
+import { Link } from '@remix-run/react';
+import { useTranslation } from 'react-i18next';
+
+export default function Component() {
+  let { t } = useTranslation();
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <h1>{t('greeting')}</h1>
+      <Link to="home">Home</Link>
+    </>
   );
 }
